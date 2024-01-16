338be2fba3bb7d025bcea03720d6329c095f905b
Saiteja-Goli <saitejagoli111@gmail.com>
2024-01-16 11:47:08 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const connection = require("./Configs/db");
 const movie_router = require("./Router/movieRouter");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "This is Backend" });
 });
 
 app.use("/movies", movie_router);
-
-app.listen(9000, async () => {
+app.listen(process.env.PORT, async () => {
   try {
     await connection;
     console.log("connected to db");
-    console.log("Connected to the Backend");
+    console.log("Server Runnning");
   } catch (error) {
     console.log(error);
   }
