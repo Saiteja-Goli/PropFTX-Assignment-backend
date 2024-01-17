d0591046ba35ee85ddc30331baa914365755fd87
Saiteja-Goli <saitejagoli111@gmail.com>
2024-01-17 13:20:42 +0530

    Updated Security

--- server/index.js
+++ server/index.js
@@ -1,26 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-
+const helmet = require("helmet"); // Adding Helmet for security headers
+const rateLimit = require("express-rate-limit"); // Adding rate limiting for DDoS protection
+const xss = require("xss-clean"); // Adding XSS protection
 require("dotenv").config();
-
-const connection = require("./Configs/db");
 const movie_router = require("./Router/movieRouter");
 
 const app = express();
 app.use(cors());
+app.use(helmet());
+
+
+
+// Apply rate limiting middleware to protect against DDoS attacks
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
 app.use(express.json());
 
+// Add XSS protection
+app.use(xss());
+
 app.get("/", (req, res) => {
+  const origin = req.get("Origin");
+  console.log("Received origin:", origin);
   res.status(200).json({ msg: "This is Backend" });
 });
 
 app.use("/movies", movie_router);
-app.listen(process.env.PORT, async () => {
-  try {
-    await connection;
-    console.log("connected to db");
-    console.log("Server Runnning");
-  } catch (error) {
-    console.log(error);
-  }
-});
+
+if (process.env.mongodb) {
+  const connection = require("./Configs/db");
+  app.listen(process.env.PORT || 4000, async () => {
+    try {
+      await connection;
+      console.log("connected to db");
+      console.log("Server Is Running");
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  });
+} else {
+  console.log("Require MongoDB URL for Starting the server");
+}
